1da70ca22f3183ccbdea7fd2024c0595ac3c7d4c
tony <narifu@163.com>
2018-03-09 20:57:43 +0800

    add datatime

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+$(function () {
+    $(".tab>button").click(function () {
+      $(this).siblings().removeClass("active");
+    })
+})
+var nowTemp = new Date();
+var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+var checkin = $('#dpd1').fdatepicker({
+    format: 'yyyy-mm-dd',
+    onRender: function (date) {
+        return date.valueOf() < now.valueOf() ? 'disabled' : '';
+    }
+}).on('changeDate', function (ev) {
+    if (ev.date.valueOf() > checkout.date.valueOf()) {
+        var newDate = new Date(ev.date)
+        newDate.setDate(newDate.getDate() + 1);
+        checkout.update(newDate);
+    }
+    checkin.hide();
+    $('#dpd2')[0].focus();
+}).data('datepicker');
+
+var checkout = $('#dpd2').fdatepicker({
+    format: 'yyyy-mm-dd',
+    onRender: function (date) {
+        return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+    }
+}).on('changeDate', function (ev) {
+    checkout.hide();
+}).data('datepicker');
